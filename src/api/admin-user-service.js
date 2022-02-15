ecfec4f51133bd1a256beee83e4e2ad0f8ff2bba
Busra <bus@example.com>
2022-02-15 11:09:08 +0300

    v1

---
+++ src/api/admin-user-service.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const getUsers = () => {
+  return axios.get(`${API_URL}user/auth/all`, {
+    headers: authHeader(),
+  });
+};
+
+const getUserById = (userId) => {
+  return axios.get(`${API_URL}user/${userId}/auth`, {
+    headers: authHeader(),
+  });
+};
+
+const createUser = (user) => {
+  return axios.post(`${API_URL}add`, user, {
+    headers: authHeader(),
+  });
+};
+
+const updateUser = (userId, user) => {
+  return axios.put(`${API_URL}user/${userId}/auth`, user, {
+    headers: authHeader(),
+  });
+};
+
+const deleteUser = (userId) => {
+  return axios.delete(`${API_URL}user/${userId}/auth`, {
+    headers: authHeader(),
+  });
+};
+
+const downloadUsers = () => {
+  return axios.get(`${API_URL}excel/download/users`, {
+    headers: {
+      ...authHeader(),
+      "Content-Type":
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    },
+    //Gelen dokumanın excel turu oldugunu gosteriyor(Tarayici). Alttaki ise backend'de gonderilen data turunu gostermekte(Axios)
+
+    responseType: "arraybuffer",
+  });
+};
+
+export {
+  getUsers,
+  createUser,
+  downloadUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
---
+++ src/router/CustomRoutes.js
@@ -0,0 +1,137 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AboutPage from "../pages/AboutPage";
+import UserEditPage from "../pages/admin/UserEditPage";
+import UsersNewPage from "../pages/admin/UsersNewPage";
+import UsersPage from "../pages/admin/UsersPages";
+import ContactPage from "../pages/ContactPage";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import NotAuthorizedPage from "../pages/NotAuthorizedPage";
+import RegisterPage from "../pages/RegisterPage";
+import ServicesPage from "../pages/ServicesPage";
+import ProfilePage from "../pages/user/ProfilePage";
+import UserReservationDetailPage from "../pages/user/UserReservationDetailPage";
+import UserReservationsPage from "../pages/user/UserReservationsPage";
+import PrivateRoute from "./PrivateRoute";
+import VehiclesPage from "../pages/admin/VehiclesPage";
+import VehiclesNewPage from "../pages/admin/VehiclesNewPage";
+import VehiclesEditPage from "../pages/admin/VehiclesEditPage";
+import ReservationsPage from "../pages/admin/ReservationsPage";
+import ReservationEditPage from "../pages/admin/ReservationEditPage";
+
+const CustomRoutes = () => {
+  return (
+    <Routes>
+      {/* Admin Routes */}
+
+      <Route
+        path="/admin/users"
+        element={
+          <PrivateRoute admin={true}>
+            <UsersPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/admin/users/new"
+        element={
+          <PrivateRoute admin={true}>
+            <UsersNewPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/admin/users/:userId"
+        element={
+          <PrivateRoute admin={true}>
+            <UserEditPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/admin/vehicles"
+        element={
+          <PrivateRoute admin={true}>
+            <VehiclesPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/admin/vehicles/new"
+        element={
+          <PrivateRoute admin={true}>
+            <VehiclesNewPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/admin/vehicles/:vehicleId"
+        element={
+          <PrivateRoute admin={true}>
+            <VehiclesEditPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/admin/reservations"
+        element={
+          <PrivateRoute admin={true}>
+            <ReservationsPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/admin/reservations/:reservationId"
+        element={
+          <PrivateRoute admin={true}>
+            <ReservationEditPage />
+          </PrivateRoute>
+        }
+      />
+
+      {/* Customer Routes */}
+
+      <Route
+        path="/reservations/:reservationId"
+        element={
+          <PrivateRoute>
+            <UserReservationDetailPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/reservations"
+        element={
+          <PrivateRoute>
+            <UserReservationsPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>
+        }
+      />
+
+      {/* Visitor Routes */}
+      <Route path="/not-authorized" element={<NotAuthorizedPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/services" element={<ServicesPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+};
+
+export default CustomRoutes;
